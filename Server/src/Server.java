a34b1b93593f958359504cb9e0d8b12ff98a9859
cacoinchik <mr.ruslan126@mail.ru>
2023-02-07 19:49:44 +0300

    Сервер может читать ответы и отправлять запросы. Создан клиент и реализовано подключение к серверу. Клиент умеет делать запрос и читать ответ

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -6,19 +6,32 @@ public class Server {
     public static void main(String[] args) {
 
         try (ServerSocket server = new ServerSocket(8000)) {
+
             System.out.println("Server started");
-            try (Socket socket = server.accept();
-                 BufferedWriter writer =
-                         new BufferedWriter(
-                                 new OutputStreamWriter(socket.getOutputStream()))) {
-                writer.write("HELLO FROM SERVER");
-                writer.flush();
-                writer.newLine();
-            }
+
+            while (true)
+                try (Socket socket = server.accept();
+                     BufferedWriter writer =
+                             new BufferedWriter(
+                                     new OutputStreamWriter(socket.getOutputStream()));
+                     BufferedReader reader =
+                             new BufferedReader(
+                                     new InputStreamReader(
+                                             socket.getInputStream()))
+                ) {
+                    String request = reader.readLine();
+                    System.out.println("Request: " + request);
+                    String response =(int) (Math.random()*30-10) + "";
+                    System.out.println("Response: " + response);
+                    writer.write(response);
+                    writer.newLine();
+                    writer.flush();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
