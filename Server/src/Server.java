473febac9d6bccdb4c4751ab32f2af94ef909af9
cacoinchik <mr.ruslan126@mail.ru>
2023-02-07 19:46:02 +0300

    Написан простой сервер автоответчик. Код упрощен с помощью 'try catch'

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,2 +1,24 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) {
+
+        try (ServerSocket server = new ServerSocket(8000)) {
+            System.out.println("Server started");
+            try (Socket socket = server.accept();
+                 BufferedWriter writer =
+                         new BufferedWriter(
+                                 new OutputStreamWriter(socket.getOutputStream()))) {
+                writer.write("HELLO FROM SERVER");
+                writer.flush();
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
